30cd76324b9cd9f4aef5c0667bd4186468ef463b
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-05-12 23:52:38 -0300

    listando os dados e limpando os campos

--- app.js
+++ app.js
@@ -47,6 +47,29 @@ class Bd{
 
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistoros(){
+        console.log('listando os dados')
+
+        // let despesas = Array();
+        let despesas = [];
+        let id = localStorage.getItem('id')
+
+        // percorrendo todos os itens usando um laço de repetição.
+        for(let i = 1; i <= id; i++){
+
+            let despesa = JSON.parse(localStorage.getItem(i))
+            // console.log(i, despesa);
+
+            // verificar indices removidos
+            if(despesa === null){
+                continue;
+            }
+            
+            despesas.push(despesa)
+        }
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -77,7 +100,19 @@ function cadastrarDespesa() {
         document.getElementById('modal_conteudo').innerHTML = 'Registrado com sucesso'
         document.getElementById('modal_btn').innerHTML = 'Voltar'
         document.getElementById('modal_btn').className = 'btn btn-success'
-		$('#modalRegistraDespesa').modal('show') 
+		$('#modalRegistraDespesa').modal('show')
+
+        /*** liga os camops */
+        function limpar(){
+            ano = document.getElementById('ano').value = ''
+            mes = document.getElementById('mes').value = ''
+            dia = document.getElementById('dia').value = ''
+            tipo = document.getElementById('tipo').value = ''
+            descricao = document.getElementById('descricao').value = ''
+            valor = document.getElementById('valor').value = ''
+        }
+        limpar()
+        window.open('http://127.0.0.1:5500/consulta.html')
 	} else {
 		//dialog de sucesso
         document.getElementById('modal_titulo').innerHTML = 'Erro não incluido !'
@@ -90,5 +125,46 @@ function cadastrarDespesa() {
 }
 
 function carregaListaDespesas(){
-    
+    let despesas = []
+    despesas = bd.recuperarTodosRegistoros()
+
+    // console.log(despesas)
+
+    var listaDespesas = document.getElementById('listaDespesas')
+    /***
+         <tr>
+            <td>15/03/2018</td>
+            <td>Alimentação</td>
+            <td>Compra do mês</td>
+            <td>1.080,00</td>
+        </tr>
+     */
+
+    despesas.forEach( function(d){
+        // console.log(d)
+
+        // criando uma linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        // criando uma linha (td)
+        // linha.insertCell(0).innerHTML = d.dia + '/' + d.mes + '/' + d.ano
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        switch (d.tipo) {
+            case '1' : d.tipo = 'Alimentação'
+            break
+            case '2' : d.tipo = 'Educação'
+            break
+            case '3' : d.tipo = 'Lazer'
+            break
+            case '4' : d.tipo = 'Saúde'
+            break
+            case '5' : d.tipo = 'Transporte'
+            break
+        }
+        linha.insertCell(1).innerHTML = `${d.tipo}`
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+
+
+    })
 }
